3bde45db16ec31a60e36f593c64cc6417c145d6d
Greyver <125139332+Greyver@users.noreply.github.com>
2023-05-01 11:59:51 +0300

    Add files via upload

---
+++ task-10.js
@@ -0,0 +1,15 @@
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// повернеться оригінальний рядок
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// повернеться форматований рядок
+
+console.log(formatString('Curabitur ligula sapien.'));
+// повернеться оригінальний рядок
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
+// повернеться форматований рядок

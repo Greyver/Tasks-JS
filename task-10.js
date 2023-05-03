d0ffd24415778284db906543dd09925fde7cb135
Greyver <125139332+Greyver@users.noreply.github.com>
2023-05-03 14:19:45 +0300

    Update task-10.js

--- task-10.js
+++ task-10.js
@@ -1,15 +1,20 @@
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
-// повернеться оригінальний рядок
+const formatString = function (string) {
+  if (string.length <= 40) { // якщо довжина менше 40 символів
+    return string; // повернути вхідний рядок без змін
+  }
+  else { // якщо довжина більше 40 символів
+    return string.slice(0, 40) + '...'; // повернути укорочену версію з додаванням трьох крапок в кінці
+  }
+};
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
-// повернеться форматований рядок
+/*
+ * Перевірка роботи функції
+ */
 
-console.log(formatString('Curabitur ligula sapien.'));
-// повернеться оригінальний рядок
+console.log(formatString('Curabitur ligula sapien, tincidunt non.')); // поверне оригінальний рядок
 
-console.log(
-  formatString(
-    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-  ),
-);
-// повернеться форматований рядок
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.')); // поверне форматований рядок
+
+console.log(formatString('Curabitur ligula sapien.')); // поверне оригінальний рядок
+
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.')); // поверне форматований рядок
